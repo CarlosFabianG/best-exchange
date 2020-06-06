9438869ecd8619e5f49b8617a9d45fb23b01e655
Carlos Fabián <carl.fab.g@gmail.com>
2020-06-06 17:51:37 -0500

    se agrego primer componente bx-header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main>
+    <bx-Header/>
+  </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import BxHeader from '@/components/BxHeader'
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+   BxHeader
   }
 };
 </script>
