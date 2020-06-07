395427d0828b6641eb603151386b01b120ef8f32
Carlos Fabián <carl.fab.g@gmail.com>
2020-06-06 19:12:55 -0500

    router-link funcionando correctamente en componente About

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home'
+import About from '@/views/About'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
             path: '/',
             name: 'home',
             component: Home
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: About
         }
     ]
 })
