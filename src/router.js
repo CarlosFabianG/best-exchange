95909bb788b3bbfe53f5411053549ad4e4183e3c
Carlos Fabián <carl.fab.g@gmail.com>
2020-06-06 19:17:48 -0500

    se agrego Error page component en views

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home'
 import About from '@/views/About'
+import Error from '@/views/Error'
 
 Vue.use(Router)
 
@@ -18,6 +19,11 @@ export default new Router({
             path: '/about',
             name: 'about',
             component: About
+        },
+        {
+            path: '*',
+            name: 'error',
+            component: Error
         }
     ]
 })
