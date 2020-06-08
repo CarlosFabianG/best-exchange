d00c8ead5f7c544ccceffda57a929d024abe807f
Carlos Fabián <carl.fab.g@gmail.com>
2020-06-08 12:11:17 -0500

    se agrego funcion detail

--- src/components/BxHeader.vue
+++ src/components/BxHeader.vue
@@ -11,7 +11,16 @@
         <div
           class="hidden sm:block w-full blok flex-grow lg:flex lg:items-center lg:wauto"
         >
-          <div class="text-sm lg:flex-grow"></div>
+          <div class="text-sm lg:flex-grow">
+            <router-link
+            v-for="l in links"
+            v-bind:key="l.title" 
+            :to="l.to"
+            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              {{ l.title }}
+            </router-link>
+          </div>
         </div>
       </nav>
     </nav>
@@ -23,6 +32,13 @@ import BxIcon from "@/components/BxIcon";
 
 export default {
   name: "BxHeader",
-  components: { BxIcon }
+  components: { BxIcon },
+
+  props: {
+    links: {
+      type: Array,
+      default: () => []
+    }
+  }
 };
 </script>
